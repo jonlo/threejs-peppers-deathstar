5f93044cdf3778cfae6da313ced173a768172e4f
Jon lopez de Guereña <jon.ldg85@gmail.com>
2019-12-20 16:48:35 +0100

    death star working

--- Src/index.js
+++ Src/index.js
@@ -1,68 +1,68 @@
 import * as THREE from 'three';
-import { WEBGL } from 'three/examples/jsm/WebGL.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { PeppersGhostEffect } from 'three/examples/jsm/effects/PeppersGhostEffect.js';
+var container;
+var camera, scene, renderer, effect, deathStar, light;
+var mixer;
+var clock = new THREE.Clock();
 
-if (WEBGL.isWebGL2Available() === false) {
-	document.body.appendChild(WEBGL.getWebGL2ErrorMessage());
-} else {
-	init();
-}
-var camera, scene, renderer, controls, container, effect;
-
-
+init();
+animate();
 function init() {
-	container = document.createElement( 'div' );
-	document.body.appendChild( container );
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+	container = document.createElement('div');
+	document.body.appendChild(container);
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100000);
 	scene = new THREE.Scene();
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	container.appendChild( renderer.domElement );
-	effect = new PeppersGhostEffect( renderer );
-	effect.setSize( window.innerWidth, window.innerHeight );
-	effect.cameraDistance = 5;
-	window.addEventListener( 'resize', onWindowResize, false );
+	// Cube
 
 
-	var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
-	scene.add(ambientLight);
-	var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-	directionalLight.position.set(1, 1, 0).normalize();
-	scene.add(directionalLight);
-	//
-
-	controls = new OrbitControls(camera, renderer.domElement);
-	controls.update();
-
+	light = new THREE.HemisphereLight(0xffffff, 0x444444,3);
+	light.position.set(0, 200, 0);
+	scene.add(light);
+	light = new THREE.DirectionalLight(0xffffff);
+	light.position.set(0, 200, 100);
+	light.castShadow = true;
+	light.shadow.camera.top = 180;
+	light.shadow.camera.bottom = - 100;
+	light.shadow.camera.left = - 120;
+	light.shadow.camera.right = 120;
+	
+	scene.add(light);
 
 	var loader = new FBXLoader();
-	loader.load( './models/death.fbx', function ( object ) {
-		object.traverse( function ( child ) {
-			if ( child.isMesh ) {
+	loader.load('./models/death.fbx', function (object) {
+		mixer = new THREE.AnimationMixer(object);
+		var action = mixer.clipAction(object.animations[0]);
+		action.play();
+		object.traverse(function (child) {
+			if (child.isMesh) {
 				child.castShadow = true;
 				child.receiveShadow = true;
 			}
-		} );
-		scene.add( object );
-	} );
+		});
+		deathStar = object;
+		deathStar.scale.set(1/200,1/200,1/200);
+		scene.add(deathStar);
+	});
+	
 
-	animate();
-
-
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio(window.devicePixelRatio);
+	container.appendChild(renderer.domElement);
+	effect = new PeppersGhostEffect(renderer);
+	effect.setSize(window.innerWidth, window.innerHeight);
+	effect.cameraDistance = 5;
+	window.addEventListener('resize', onWindowResize, false);
 }
-
 function onWindowResize() {
-	camera.aspect = container.offsetWidth / container.offsetHeight;
+	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	effect.setSize( window.innerWidth, window.innerHeight );
-
+	effect.setSize(window.innerWidth, window.innerHeight);
 }
-
 function animate() {
 	requestAnimationFrame(animate);
-	controls.update()
-	effect.render( scene, camera );
+	var delta = clock.getDelta();
+	if (mixer) mixer.update(delta);
+	effect.render(scene, camera);
+
 }
